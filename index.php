80a2ff825c32ee53aaa73e9ab028a26988b1ffd7
Nadil Karunarathna <nadilkarunaratna2000@gmail.com>
2025-05-24 13:50:17 +0530

    wip

--- index.php
+++ index.php
@@ -18,7 +18,11 @@
         <div class="container">
             <textarea placeholder="Enter your text here..."></textarea>
             <button type="button">Correct</button>
-            <textarea placeholder="Corrected text will appear here..."></textarea>
+            <textarea placeholder="Corrected text will appear here..." readonly></textarea>
+        </div>
+
+        <div class="wave-background">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#b3cde0" fill-opacity="1" d="M0,128L60,144C120,160,240,192,360,186.7C480,181,600,139,720,106.7C840,75,960,53,1080,53.3C1200,53,1320,75,1380,85.3L1440,96L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
         </div>
     </body>
 
