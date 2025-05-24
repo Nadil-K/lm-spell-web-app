7cc584ed6ba8b46e6d14b06b00057be50b23a4ea
Nadil Karunarathna <nadilkarunaratna2000@gmail.com>
2025-05-24 12:14:40 +0530

    init

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>LM Spell</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <nav class="navbar">
+        <div class="navbar-left">
+            <div class="navbar-logo">
+                <img src="assets/uom.png" alt="Logo" id="uom-logo">
+            </div>
+            <div class="navbar-logo">
+                <img src="assets/cse.webp" alt="Logo" id="cse-logo">
+            </div>
+            <span class="navbar-title">LM Spell</span>
+        </div>
+        <div class="navbar-right">
+            <a href="https://github.com/Nadil-K/lm-spell" class="navbar-link" target="_blank">
+                <img src="assets/github.svg" alt="GitHub Logo" id="github-logo">
+                Library
+            </a>
+            <a href="about.php" class="navbar-link">About Us</a>
+        </div>
+    </nav>
+
+    <!-- <div class="container"> -->
+        <h1>LM Spell</h1>
+        <div class="container">
+            <textarea placeholder="Enter your text here..."></textarea>
+            <button type="button">Correct</button>
+            <textarea placeholder="Corrected text will appear here..."></textarea>
+        </div>
+    <!-- </div> -->
+</body>
+
+</html>
