55f5d12e9ff088d38c3283957a48542c4f6c2bfe
Nadil Karunarathna <nadilkarunaratna2000@gmail.com>
2025-05-28 00:43:25 +0530

    env moved to backend

--- backend/LmSpell.py
+++ backend/LmSpell.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 import os
 
-dotenv_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), '.env')
+dotenv_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '.env')
 load_dotenv(dotenv_path)
 
 class LmSpell:
